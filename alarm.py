6888abe531dd7386cf1e4342745f1847ac7c9f48
parisaDuets <p.abbasi@duets.ai>
2023-11-09 16:25:11 -0800

    Revert "Done"
    This reverts commit 84d2912290dc3550a49abd96c39bb1080794cafc.

--- alarm.py
+++ alarm.py
@@ -15,11 +15,11 @@ from pgmpy.factors.discrete import TabularCPD
 
 cpd_burglary = TabularCPD(
     variable="Burglary", variable_card=2, values=[[0.999], [0.001]],
-    state_names={"Burglary": ['no', 'yes']},
+    state_names={"Burglary":['no','yes']},
 )
 cpd_earthquake = TabularCPD(
     variable="Earthquake", variable_card=2, values=[[0.998], [0.002]],
-    state_names={"Earthquake": ["no", "yes"]},
+    state_names={"Earthquake":["no","yes"]},
 )
 cpd_alarm = TabularCPD(
     variable="Alarm",
@@ -27,7 +27,7 @@ cpd_alarm = TabularCPD(
     values=[[0.999, 0.71, 0.06, 0.05], [0.001, 0.29, 0.94, 0.95]],
     evidence=["Burglary", "Earthquake"],
     evidence_card=[2, 2],
-    state_names={"Burglary": ['no', 'yes'], "Earthquake": ['no', 'yes'], 'Alarm': ['yes', 'no']},
+    state_names={"Burglary":['no','yes'], "Earthquake":['no','yes'], 'Alarm':['yes','no']},
 )
 cpd_johncalls = TabularCPD(
     variable="JohnCalls",
@@ -35,7 +35,7 @@ cpd_johncalls = TabularCPD(
     values=[[0.95, 0.1], [0.05, 0.9]],
     evidence=["Alarm"],
     evidence_card=[2],
-    state_names={"Alarm": ['yes', 'no'], "JohnCalls": ['yes', 'no']},
+    state_names={"Alarm":['yes','no'], "JohnCalls":['yes', 'no']},
 )
 cpd_marycalls = TabularCPD(
     variable="MaryCalls",
@@ -43,7 +43,7 @@ cpd_marycalls = TabularCPD(
     values=[[0.1, 0.7], [0.9, 0.3]],
     evidence=["Alarm"],
     evidence_card=[2],
-    state_names={"Alarm": ['yes', 'no'], "MaryCalls": ['yes', 'no']},
+state_names={"Alarm":['yes','no'], "MaryCalls":['yes', 'no']},
 )
 
 # Associating the parameters with the model structure
@@ -52,19 +52,7 @@ alarm_model.add_cpds(
 
 alarm_infer = VariableElimination(alarm_model)
 
+print(alarm_infer.query(variables=["JohnCalls"],evidence={"Earthquake":"yes"}))
+q = alarm_infer.query(variables=["JohnCalls", "Earthquake"],evidence={"Burglary":"yes","MaryCalls":"yes"}))
+print(q)
 
-def print_alarm_cpd(cpd):
-    # Perform inference on the Bayesian Network
-    query1_result = alarm_infer.query(variables=["MaryCalls"], evidence={"JohnCalls": "yes"})
-    query2_result = alarm_infer.query(variables=["JohnCalls", "MaryCalls"], evidence={"Alarm": "yes"})
-    query3_result = alarm_infer.query(variables=["Alarm"], evidence={"MaryCalls": "yes"})
-
-    # Print the results
-    print("Probability of Mary making a call given that John has called: ")
-    print(query1_result)
-
-    print("Probability of John and Mary both making calls when the alarm has gone off: ")
-    print(query2_result)
-
-    print("Probability of the alarm going off when Mary has made a call: ")
-    print(query3_result)
